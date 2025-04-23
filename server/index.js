96f05a7d350f73f9e0e6197381dd5d714bdcacbe
zhuochen777 <zhuochen777@gmail.com>
2025-04-23 15:57:47 -0700

    fetch and filter products

--- server/index.js
+++ server/index.js
@@ -6,9 +6,11 @@ const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
-
+const cors = require("cors")
 
 const app = express();
+app.use(cors())
+
 
 dotenv.config();
 
